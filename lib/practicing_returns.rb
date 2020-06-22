494b7a514ca62c6958ba595c1563b72a5b42b940
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-22 23:11:06 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,9 +3,10 @@
 # hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    binding.pry
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
